469a8a43b9cc19768be5df06f90687755608712b
nick thomas <nichoth@gmail.com>
2023-04-24 14:50:14 -0700

    works

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,61 @@
-export function example ():void {
-    console.log('hello')
+import { webcrypto } from 'one-webcrypto'
+import { toString } from 'uint8arrays/to-string'
+import _cloudinary from 'cloudinary'
+const cloudinary = _cloudinary.v2
+
+interface cloudinaryParams {
+    cloudName:string,
+    apiKey:string,
+    apiSecret:string
 }
+
+export class BlobStore {
+    cloudinary
+
+    static Cloudinary (params?:cloudinaryParams) {
+        const config = (params ? ({
+            cloud_name: params.cloudName,
+            api_key: params.apiKey,
+            api_secret: params.apiSecret
+        }) : ({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        }))
+
+        return new BlobStore(config)
+    }
+
+    constructor (config) {
+        cloudinary.config(config)
+        this.cloudinary = cloudinary
+    }
+
+    async write (file) {
+        const hash = await getHash(file)
+        const slugifiedHash = encodeURIComponent(hash)
+
+        return new Promise((resolve, reject) => {
+            this.cloudinary.uploader.upload(file, {
+                public_id: slugifiedHash
+            }, function (err, res) {
+                if (err) return reject(err)
+                resolve({ hash: hash, slug: slugifiedHash, response: res })
+            })
+        })
+    }
+}
+
+// data should be Uint8Array
+export function getHash (data) {
+    let _data = data
+    if (typeof data === 'string') {
+        const te = new TextEncoder()
+        _data = te.encode(data)
+    }
+
+    return webcrypto.subtle.digest('SHA-256', _data).then(buf => {
+        return toString(new Uint8Array(buf), 'base64url')
+    })
+}
+
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,22 @@
 import { test } from 'tapzero'
+import { BlobStore } from '@ssc-hermes/blob-store'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+// cloud_name: params.cloudName,
+// api_key: params.apiKey,
+// api_secret: params.apiSecret
+
+test('create cloudinary blob store without passing params', t => {
+    const store = BlobStore.Cloudinary()
+    t.ok(store, 'create a store')
+})
+
+test('create a blob store with params', t => {
+    const store = BlobStore.Cloudinary({
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME as string,
+        apiKey: process.env.CLOUDINARY_API_KEY as string,
+        apiSecret: process.env.CLOUDINARY_API_SECRET as string
+    })
+    t.ok(store, 'create a store')
 })
